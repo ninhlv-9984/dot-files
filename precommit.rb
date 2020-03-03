ef729b68f8bc83ea9efb25cc63ed647faa7bb6d4
Ninh <le.van.ninh@sun-asterisk.com>
2020-03-03 15:32:38 +0700

    add git precommit rubocop check

---
+++ precommit.rb
@@ -0,0 +1,6 @@
+git_files = `git status --porcelain`
+ruby_files = git_files.scan(%r{[M\?] ([\w\/]+.rb)}).flatten.select { |file| !file.match(/routes|migrate/) }
+ruby_files_list = ruby_files.join(" ")
+
+system("rubocop #{ruby_files_list}")
+
