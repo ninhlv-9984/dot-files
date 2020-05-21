732ad64c5ec9dbb0c506d45bcf38ff87474d10b7
oLeVanNinh <ninhle.9984@gmail.com>
2020-05-21 14:53:06 +0700

    Update precommit.rb

--- precommit.rb
+++ precommit.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
 git_files = `git status --porcelain`
-ruby_files = git_files.scan(%r{[M\?] ([\w\/]+.rb)}).flatten.select { |file| !file.match(/routes|migrate/) }
+ruby_files = git_files.scan(%r{[M\?]\s+([\w\/]+.rb)}).flatten.reject { |file| file.match(/routes|migrate/) }
 ruby_files_list = ruby_files.join(" ")
 
-system("rubocop #{ruby_files_list}")
-
+system("rubocop #{ruby_files_list}") unless ruby_files_list.empty?
